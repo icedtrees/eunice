3945338594a04b189fac4150647ad923b3063c66
Davy <e_equals_mass_speed_light_squared@hotmail.com>
2017-12-15 23:59:13 -0800

    make button spawn image

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function click() {
   alert("hello");
+  var img = document.createElement('img');
+  img.setAttribute('src', 'banner.png');
+  document.body.appendChild(img);
 }
 
 window.onload = function () {
