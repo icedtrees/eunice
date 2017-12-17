0f803478525d51c38a3b420f29dfca1a2a668785
Davy <e_equals_mass_speed_light_squared@hotmail.com>
2017-12-16 20:37:40 -0800

    add basic content file

---
+++ content.js
@@ -0,0 +1,31 @@
+var IMAGE_TYPE = 'IMAGE_TYPE';
+var TEXT_TYPE = 'TEXT_TYPE';
+
+var historicalQuotes = [
+  `
+blog review:
+fail whale.
+Author: Happy Apple/Eunice
+Theme: Grey text on various shades of green. Peaceful green wheat-pictured background.
+Followers: AEreas, Crystal, littlemishsunshine, btan, icedtrees, Sweet(:, Ophelia, Bitter ^^, Elisa, Beccie Tsao, Renee, Joanna, delete12, Riku, we're nothing like you, Daniel W, Srisha's bloog, Neku Sakuraba, kiss_bliss_butterfly, Harvard, JB-HI5, Cookie Dough, ~dandelion*, Aarun, human.jukebox, Average, JM
+Unique aspects: Recent comments! Haven't seen that in any other blogs. A nice followers tab! Maybe I'll put this in my followers tab. Happy Apple tips and facts!
+Review: Bright theme makes posts seem brighter when you're reading them and thinking about them. The posts themselves are pretty happy as well. "Happy Apple" really seems to suit her - happy because of the aura she exudes both in public and on her blog, and Apple because it reminds me of "an apple a day" - consistent dedication to personal health and also a responsible dedication to study. Posts daily, and always provides something interesting to read. Excited/enthusiastic perspective on everything that happens. Admirable dedication to medicine. Can't find a better word to describe it than "nice".
+`,
+  'you are further down than the guy up there!',
+  '10/10 would do again',
+  'The tree is our next goal!',
+];
+
+var chibirdImages = [
+  {
+    'type': IMAGE_TYPE,
+    'content': [
+      'rainbow.jpg',
+      'caption' + `life is really hard sometimes. me, most of the time i sit in my house wasting my time doing nothing. ive spent months and months just playing games (not even good games), reading reddit, watching youtube, and basically making a waste out of my life. but once in a blue moon i feel motivated, and i feel like life is back to being something worth fighting for. i'd better make the most of this. tbh maybe being unmotivated is the default state. i know u feel unmotivated sometimes. i think it's fine to just chill and do things u like and hang with people u like until u feel ready to fight again. eunice hwaiting!`,
+      'stronger-life.jpg',
+      `yeah, and ur life has been getting harder ever since. not only did you have to deal with your ed, u had to perform perfectly in the hsc, get into med, pass radiography, get into med again, and pass med exams - all while dealing with a ton of shit. what a life man. u never signed up for any of this. except med i guess, u signed yourself up for that. congrats again on getting into med! maybe one day u'll find something else worth fighting for.`,
+      'rain-sunshine-rainbow.jpg',
+      `i feel like some of the best parts of my life have been transitions from things like my junior high school to baulko. only way is up, right?`
+    ]
+  }
+];
